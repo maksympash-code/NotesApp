ceb15e656ee893ba356c729098a3149a29360aec
maksympash <maksympash@gmail.com>
2026-06-20 20:17:00 +0300

    Refactoring

--- app/src/main/java/ua/knu/maksym_pashchenko/notesapp/presentation/notes/elements/SearchField.kt
+++ app/src/main/java/ua/knu/maksym_pashchenko/notesapp/presentation/notes/elements/SearchField.kt
@@ -1,12 +1,10 @@
 package ua.knu.maksym_pashchenko.notesapp.presentation.notes.elements
 
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun SearchField(
@@ -18,8 +16,7 @@ fun SearchField(
         value = query,
         onValueChange = onSearchQueryChanged,
         modifier = modifier
-            .fillMaxWidth()
-            .padding(top = 8.dp),
+            .fillMaxWidth(),
         singleLine = true,
         label = {
             Text("Search")
